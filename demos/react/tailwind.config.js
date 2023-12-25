324a8367159410a433203e903749e11c6c0e7284
c4ffein <c4ffein>
2023-12-25 21:44:00 +0100

    tailwind.config.js

---
+++ demos/react/tailwind.config.js
@@ -0,0 +1,18 @@
+import { tailwindExtract } from './xjsx';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: {
+    files: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    extract: tailwindExtract,
+  },
+  theme: {
+    extend: {
+      colors: {
+        react: '#61dafb',
+        github: '#0d1117',
+      },
+    },
+  },
+  plugins: [],
+};
